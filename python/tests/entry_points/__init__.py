b871fca3f1e8cc1e5bbdde4b377d2c581c076d08
Keys <37385516+etkeys@users.noreply.github.com>
2021-01-31 12:11:54 -0500

    unify use of subprocess in tests (#41)
    Added wrapper method to standarize use of `subprocess.run` across all
    tests that currently use this method.
    
    Resolves #26

--- python/tests/entry_points/__init__.py
+++ python/tests/entry_points/__init__.py
@@ -1,9 +1,19 @@
+import subprocess
+
 import tests.utils as utils
 
 
 def _delete_dir(step):
     if "delete_dir" in step:
         utils.delete_dir(step.delete_dir)
+
+
+def call_subprocess(cmd_parts):
+    act = subprocess.run(
+        cmd_parts, stderr=subprocess.PIPE, stdout=subprocess.PIPE, encoding="utf-8"
+    )
+
+    return (act.returncode, {"stderr": act.stderr, "stdout": act.stdout})
 
 
 def setup(data):
