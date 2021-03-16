d9b02a1fc8bce9790703b03543d51c5b82b5c0d1
E. Keys <e.t.keys@gmail.com>
2021-03-16 17:32:56 -0400

    add tests for GaussianSolver.__init__
    Resolves #56

--- python/tests/solvers/test_gaussian.py
+++ python/tests/solvers/test_gaussian.py
@@ -1,11 +1,28 @@
 from math import isclose
 
-from src.matrix_operations import almost_equal
+from src.matrix_operations import almost_equal, reshape
 from src.solvers import GaussianSolver
 from tests.utils import create_matrix
 from . import common_test_solve
 
-# TODO need to test for __init__
+
+def test_init(name, data, exception):
+    inp_matA = create_matrix(data.input.matA)
+    inp_matb = create_matrix(data.input.matb)
+
+    with exception:
+        actor = GaussianSolver(matA=inp_matA, matb=inp_matb)
+        act_matA = actor._matA
+        act_matb = actor._matb
+        act_mat = actor._mat
+
+        exp_matA = create_matrix(data.expect.matA)
+        exp_matb = create_matrix(data.expect.matb)
+        exp_mat = create_matrix(data.expect.mat)
+
+        assert almost_equal(act_matA, exp_matA)
+        assert almost_equal(act_matb, exp_matb)
+        assert almost_equal(act_mat, exp_mat)
 
 
 def test_calc_back_solve_vector_row(name, data, exception):
