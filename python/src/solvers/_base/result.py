d6536ac2488cbf199181f6e115b5fac041390074
E. Keys <e.t.keys@gmail.com>
2021-03-16 19:54:56 -0400

    code cleanup
    - Cleaned up outstanding TODO comments.
    - Added back SOR divergent tests for `solve` action.
    - Renamed `.scripts` directory to `scripts` and updated CI steps.
    - Removed unused methods in solver Result.
    
    Resolves #20

--- python/src/solvers/_base/result.py
+++ python/src/solvers/_base/result.py
@@ -23,43 +23,16 @@ class Result(MutableMapping):
         return ret
 
     def __iter__(self):
-        keys = set(self._data.keys())
-        # The attributes that must appear first and in this order!!!
-        first = [
-            ResultAttributes.RESULT_VECTOR,
-            ResultAttributes.ERROR,
-        ]
-        ikeys = []
-        for k in first:
-            if k in keys:
-                ikeys.append(k)
-                keys.remove(k)
-        ikeys.extend(keys)
-        self._iter_keys = ikeys
-        self._iter_curr = -1  # need to increment this before accessing
-        self._len = (
-            len(ikeys) - 1
-        )  # account for strange increment style, whatever that is
-        return self
+        return iter(self._data)
 
     def __len__(self):
         return len(self._data)
-
-    def __next__(self):
-        return self.next()
 
     def __repr__(self):
         return repr(self._data)
 
     def __setitem__(self, k: ResultAttributes, v) -> None:
         self._data[k] = v
-
-    def next(self):
-        if self._iter_curr < self._len:
-            self._iter_curr += 1
-            ret_key = self._iter_keys[self._iter_curr]
-            return self._data[ret_key]
-        raise StopIteration
 
     @property
     def has_error(self):
